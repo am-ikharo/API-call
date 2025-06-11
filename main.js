18d1acca32c1f62c68bc58588018ad77d906deb2
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-11 01:25:10 +0100

    created html and css files

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+
+
+
+const postsListContainer = document.querySelector('.poss-list-container')
+
+// fetch using XHR
+
+function fetchUsingXHR(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.responseType = 'json';
+    xhr.send();
+
+    xhr.onload = () => {
+        if(xhr.status === 200 ){
+            console.log(xhr);
+            
+        }
+    } 
+}
+
+fetchUsingXHR()
