e7c4997fb6a054403d0fae3bb6fb8046b0119d6a
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-12 23:40:16 +0100

    created the xhr function to get the xml response

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 
 
 
-const postsListContainer = document.querySelector('.poss-list-container')
+const postsListContainer = document.querySelector('.post-list-container')
 
 // fetch using XHR
 
-function fetchUsingXHR(){
+function fetchUsingXHR() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts')
     xhr.responseType = 'json';
     xhr.send();
 
     xhr.onload = () => {
-        if(xhr.status === 200 ){
-            console.log(xhr);
+        if(xhr.status === 200){
+            displayResults(xhr.response)
+            
+        }else{
+            console.log('some error occured');
             
         }
-    } 
+    }
 }
 
 function displayResults(posts){
-
+    postsListContainer.innerHTML = posts.map(
+        (postItem) => `
+        <div class='post-item'>
+        <h3>${postItem.title}</h3>
+        <p>${postItem.body}</p>
+        </div>
+        `
+    ).join(' ')
 }
 
-fetchUsingXHR()
+fetchUsingXHR();
